efafe530f6e36584eb86f7fa1c95fb4bc6449afe
vmphung <phung.vo@kyanon.digital>
2023-12-07 00:59:02 +0700

    [Deployment] Update source before progress deployment to vercel

--- components/Organisms/MainboardStatus/Waiting/index.tsx
+++ components/Organisms/MainboardStatus/Waiting/index.tsx
@@ -1,34 +1,13 @@
-import {
-  Breadcrumb,
-  Button,
-  SelectBox,
-  SnipperRound,
-} from "@/components/Atoms";
-import Image from "next/image";
+import { SelectBox, SnipperRound } from "@/components/Atoms";
 import { FC, useEffect, useState } from "react";
 import { IClassroomObject } from "@/interface/classroom";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { IAuthObject } from "@/interface/auth";
-import { unsubscribeState } from "@/redux/reducer/auth/api";
-import { useAppDispatch } from "@/redux/store";
-import { useCurrentUser } from "@/hooks/useGetCurrentUser";
-import classNames from "classnames";
-import {
-  EmptySpace,
-  FilterScheduledForm,
-  ModalConfirm,
-} from "@/components/Molecules";
+import { FilterScheduledForm } from "@/components/Molecules";
 import { ClassroomCard } from "../..";
 import { useSubscribeStateContext } from "@/contexts/subscribeState";
 import { IMemberObject } from "@/interface/member";
 import { ICategoryObject } from "@/interface/category";
 import { IOptionItem } from "@/interface/filter";
-import {
-  BREADCRUMB_MAINBOARD,
-  DATA_FILTER_COURSE,
-  DATA_FILTER_TOPICS,
-} from "../mock-data";
-import { useCurrentUserContext } from "@/contexts/currentUserContext";
+import { DATA_FILTER_COURSE, DATA_FILTER_TOPICS } from "../mock-data";
 
 export interface IWaitingViewProps {
   classroom?: IClassroomObject;
@@ -36,46 +15,7 @@ export interface IWaitingViewProps {
 
 export const WaitingView: FC<IWaitingViewProps> = ({ classroom }) => {
   const [loading, setLoading] = useState<boolean>(true);
-  const dispatch = useAppDispatch();
-  const queryClient = useQueryClient();
-  const { currentUser } = useCurrentUserContext();
   const { subscribeState } = useSubscribeStateContext();
-  const deleteMutation = useMutation(
-    (postData: IAuthObject) => {
-      return new Promise((resolve, reject) => {
-        dispatch(unsubscribeState(postData))
-          .unwrap()
-          .then((data) => {
-            resolve(data);
-          })
-          .catch((error) => {
-            reject(error);
-          });
-      });
-    },
-    {
-      onSuccess: () => {
-        queryClient.invalidateQueries([
-          "checkAuthRoleForClassroomState",
-          currentUser,
-        ]);
-      },
-    }
-  );
-
-  const [openModalConfirm, setOpenModalConfirm] = useState<boolean>(false);
-  const modalClassConfirm = classNames({
-    "modal modal-bottom sm:modal-middle": true,
-    "modal-open": openModalConfirm,
-  });
-
-  const handleOpenModalConfirm = () => {
-    setOpenModalConfirm?.(!openModalConfirm);
-  };
-
-  const handleUnsubscribeState = () => {
-    deleteMutation.mutate(currentUser);
-  };
   const [filterCourse, setFilterCourse] = useState<
     IOptionItem | ICategoryObject
   >({
@@ -127,15 +67,6 @@ export const WaitingView: FC<IWaitingViewProps> = ({ classroom }) => {
               );
             })}
           </div>
-          <ModalConfirm
-            modalClass={modalClassConfirm}
-            setOpenModal={setOpenModalConfirm}
-            openModal={openModalConfirm}
-            action={handleUnsubscribeState}
-            typeButton="subscribe"
-            title="Message!!!"
-            message="Do you want to unsubscribe this classroom"
-          />
         </>
       )}
     </>
