a05ea415b9c3e5a45c6707978c213a5176c32bf1
vmphung <phung.vo@kyanon.digital>
2023-12-07 00:51:12 +0700

    [Deployment] Update source before progress deployment to vercel

--- config/firebase-config.ts
+++ config/firebase-config.ts
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider, getAuth } from "firebase/auth";
-import dotenv from 'dotenv';
-dotenv.config();
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
